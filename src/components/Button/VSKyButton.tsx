75de7de76176d662b49d075da51aff6d069a5871
ggamsang <w.ggmsng@gmail.com>
2024-10-26 03:40:26 +0900

    update: eslint

--- src/components/Button/VSKyButton.tsx
+++ src/components/Button/VSKyButton.tsx
@@ -1,5 +1,5 @@
-import React from "react";
-import { styled } from "@stitches/react";
+import { styled } from '@stitches/react'
+import React from 'react'
 
 const Button = styled('button', {
   backgroundColor: '#18B2FF', // todo - blue로 정의하고 바꾸기
@@ -13,15 +13,15 @@ const Button = styled('button', {
   '&:hover': {
     backgroundColor: 'darkblue', // todo - hex변경하기
   },
-});
+})
 
 type ButtonProps = {
-  children: React.ReactNode;
-  onClick?: () => void;
-};
-
-function VSkyButton({ children, onClick }: ButtonProps) {
-  return <Button onClick={onClick}>{children}</Button>;
+  children: React.ReactNode
+  onClick?: () => void
 }
 
-export default VSkyButton;
+function VSkyButton({ children, onClick }: ButtonProps) {
+  return <Button onClick={onClick}>{children}</Button>
+}
+
+export default VSkyButton
